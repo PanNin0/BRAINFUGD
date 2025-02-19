8b91f8dd210b6bbaf6d51dd2bfa07b8d412ed90f
PanNino <146346484+PanNin0@users.noreply.github.com>
2025-02-19 15:06:23 -0600

    Update BRAINFUGD.py

--- BRAINFUGD.py
+++ BRAINFUGD.py
@@ -1,17 +1,6 @@
 # ---------------------------
 
-# Brainfugd v2.2.1
-
-# ---------------------------
-
-# Brief Explanation
-
-# This is BRAINFUGD, a programming language in python based off the original programming BRAINFUCK.
-# The difference between this and that is that this is entirely run and coded in through your python terminal and unlike brainfuck 
-# This comes with a few handy dandy extra commands that should make your time using this a little easier 
-# (Brainfuck originally had 8, this one has 19). 
-# If you make anything cool using this please send me an email of what you've made at BreadBoy399@gmail.com 
-# !!!DO NOT ENJOY!!!
+# Brainfugd v1.0
 
 # ---------------------------
 
